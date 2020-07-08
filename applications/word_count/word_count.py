4617124cf7f60235430fa242fccf77fb16fa7ca1
cedric-winbush <cawinbushjr@gmail.com>
2020-07-08 15:48:59 -0500

    completes word count --with return empty dictionary if no ignored characters -- this also makes test not pass

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -2,15 +2,20 @@ import re
 def word_count(s):
     # Your code here
     counts = {}
+    remove_count = 0
     original_string = s
     characters_remove = ['"', ':', ';', ',', '.', '-', '+', '=', '/',  '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
   
   # print(re.sub("|".join(characters_remove), "", original_string))
     for char in characters_remove: 
-        original_string = original_string.replace(char, '')
+        new_string = original_string.replace(char, '')
+        if len(original_string) != len(new_string):
+            remove_count += 1
+
+        original_string = new_string
 
 
-    if not original_string:
+    if remove_count == 0:
       return counts
     words = original_string.lower().split()
 
@@ -27,6 +32,7 @@ def word_count(s):
 
 if __name__ == "__main__":
     print(word_count('+*-*-*-*-[}]{"'))
-    print(word_count("Hello"))
+    print(word_count("Hello+"))
+    print(word_count("Hello    hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
