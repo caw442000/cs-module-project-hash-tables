0ab2f7ca668dc65ce763290c9c098ea86b586747
cedric-winbush <cawinbushjr@gmail.com>
2020-07-09 13:42:27 -0500

    completes no_dups and lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import random
+import math
 
 
 def slowfun_too_slow(x, y):
@@ -15,6 +17,14 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    key = (x, y)
+    cache = {}
+    
+
+    if key not in cache: 
+        cache[key] = slowfun_too_slow(x,y)
+
+    return cache[key]
 
 
 
